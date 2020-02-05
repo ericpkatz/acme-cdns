a16629f6a0c2bfd02224fb969fd2d442c775e2c1
ericpkatz <ericpkatz@gmail.com>
2020-02-04 20:35:27 -0500

    inertial kermit

---
+++ app.js
@@ -0,0 +1,39 @@
+const API = 'https://acme-users-api-rev.herokuapp.com/api';
+const nav = document.querySelector('ul');
+
+const dataPromise = Promise.all([
+  axios.get(`${API}/products`),
+  axios.get(`${API}/companies`)
+])
+.then( responses => {
+  return {
+    products: responses[0].data,
+    companies: responses[1].data
+  };
+});
+
+dataPromise.then( results => {
+  renderNav(results);
+  //renderTable(results);
+});
+
+const renderNav = (results)=> {
+  const { products, companies } = results;
+  const html = `
+    <li class='nav-item'>
+      <a class='nav-link' href='#products'>
+        Products (${ products.length })
+      </a>
+    </li>
+    <li class='nav-item'>
+      <a class='nav-link' href='#companies'>
+        Companies (${ companies.length })
+      </a>
+    </li>
+  `;
+  nav.innerHTML = html;
+};
+
+window.addEventListener('hashchange', ()=> {
+  dataPromise.then( results => console.log(results));
+});
